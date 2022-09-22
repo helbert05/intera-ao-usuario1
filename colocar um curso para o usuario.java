6e74fb1514675efade9175499f6f3ee859b40031
Helbert016 <51796477+helbert05@users.noreply.github.com>
2022-09-22 12:06:05 -0300

    Add files via upload

---
+++ colocar um curso para o usuario.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import package1.Gradebook;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+    
+    Gradebook p1 = new Gradebook();
+    Scanner falar = new Scanner(System.in);
+    System.out.println("Digite o nome do curso");
+     String NomeDoCurso = falar.nextLine();
+    System.out.println("Nome do curso e:" + NomeDoCurso);
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
